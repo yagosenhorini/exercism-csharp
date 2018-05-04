2f777c1202a309949017542953767fc9d814e84d
yagosenhorini <yagosenhorini30@gmail.com>
2018-05-04 17:39:49 -0300

    Implements Select method of Linq lib

--- bob/Bob.cs
+++ bob/Bob.cs
@@ -1,15 +1,25 @@
 using System;
+using System.Text.RegularExpressions;
 
 public static class Bob
 {
     public static string Response(string statement)
     {
-        if(statement.Trim() == ""){
+        if (statement.Trim().Equals(""))
+        {
             return "Fine. Be that way!";
         }
-        else if (statement.EndsWith("?")){
+
+        if (statement.Trim().EndsWith("?"))
+        {
             return "Sure.";
-        } 
+        }
+
+      if(statement.ToUpper().EndsWith("?") || statement.ToUpper().EndsWith("!"))
+        {
+            return "Whoa, chill out!";
+        }
+
         return "Whatever.";
 
     }
---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        var cont = 0;
+        var test = multiples.Select(e => e / e == 0 ? e : 0).Sum();
+        return test;
+
+        foreach(var item in multiples){
+            if(item / item == 0){
+                cont += item; 
+            }
+        }
+
+    }
+}
